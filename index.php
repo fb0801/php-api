4c0ae88562c9fba8b81ad5231450ac08eff86c1d
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-27 11:45:47 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -17,6 +17,26 @@
 <form class="form-inline" action="">
   <label for="stock name">Stock name:</label>
   <input type="text" class="form-control" placeholder="Enter stock symbol or name" id="stock_name">
+  <label for="Region">Stock Region:</label>
+  <input type="text" class="form-control" placeholder="Enter stock symbol or name" id="stock_name">
+
+
+  <div class="form-group">
+        <label id='lbl_5' for="sel1">Position (select one):</label>
+        <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-map-marker"data-toggle="tooltip" title="Select the stock Region" ></i></span>
+        <select class="form-control" id="sel1" name='acc_type'>
+          <option value='content creator'>Content creator</option>
+          <option value ='admin'>Admin</option>
+          <option value='Moderator'>Moderator</option>
+          <option  value='Broker'>Broker</option>
+
+        </select>
+        <br>
+
+
+      </div></div>
+
 
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
